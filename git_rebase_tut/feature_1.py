9773b690761d87bc2b4ddf1eb714cb081c9b7608
sahil <sdadia@cameramatics.com>
2022-08-18 11:06:56 +0100

    Commit - Feature 3

--- git_rebase_tut/feature_1.py
+++ git_rebase_tut/feature_1.py
@@ -1,7 +1,10 @@
-from  main import  func_add2
+from  main import  func_add2, func_add3_from_main
 
 def func_add3(x, y, z):
     return func_add2(z, func_add2(x,y))
 
 
 print(func_add3(1,2 ,3))
+
+def test_func_add3_from_main():
+    assert func_add3_from_main(1, 2, 3) == 6
